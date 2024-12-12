a0ff1c5f4c1a348d2720e7c9d64435a345f7e41e
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-12-13 00:56:50 +0700

    10

--- Dynamic_Programming/Removing_Digits.cpp
+++ Dynamic_Programming/Removing_Digits.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 #define ll long long
+const int mod = 1e9+7, mx = 1e6;
+int n;
+ll dp[mx+1];
 
 int main(){
+    cin >> n;
+    for(int i = 1; i <= n;i++){
+        dp[i] = 1e9;
+        int i2 = i;
+        while(i2) {
+            dp[i] = min(dp[i], dp[i - i2 % 10] + 1);
+            i2 /= 10;
+        }
+    }
+    cout << dp[n];
     return 0;
 }
