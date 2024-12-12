7df9da27b3568d1beeabd0884ef1ddf3449a1007
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-12-12 22:04:49 +0700

    6

--- Dynamic_Programming/Dice_Combinations.cpp
+++ Dynamic_Programming/Dice_Combinations.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 #define ll long long
+const int mod = 1e9 + 7, mx = 1e6;
+ll dp[mx+1];
 
 int main(){
+    int n;
+    cin >> n;
+    dp[0] = 1;
+    for(int i = 1;i <= n;i++){
+        for(int j = 1; j<=min(6,i);j++){
+            dp[i] = (dp[i] + dp[i-j])%mod;
+        }
+    }
+    cout << dp[n] << endl;
     return 0;
 }
