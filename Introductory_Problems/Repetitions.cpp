25d94e4df5d6aeb69d667a5ee83bbcd37032a87c
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-12-12 19:45:56 +0700

    3

--- Introductory_Problems/Repetitions.cpp
+++ Introductory_Problems/Repetitions.cpp
@@ -5,5 +5,19 @@ using namespace std;
 #define ll long long
 
 int main(){
+    string s;cin >> s;
+    int ans = 0, cnt = 0;
+    char temp = s[0];
+    for(char d: s) {
+        if(d == temp) {
+            cnt++;
+            ans = max(ans, cnt);
+        }
+        else {
+            cnt = 1;
+            temp = d;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
