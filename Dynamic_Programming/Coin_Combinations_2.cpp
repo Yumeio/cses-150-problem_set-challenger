2d9b04cb103aadda2ee21bfeddc486e09a7a9ed2
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-12-13 00:31:21 +0700

    9

--- Dynamic_Programming/Coin_Combinations_2.cpp
+++ Dynamic_Programming/Coin_Combinations_2.cpp
@@ -1,9 +1,25 @@
 #include<bits/stdc++.h> 
 
 using namespace std;
-
-#define ll long long
+#define ll long long 
+const int mod = 1e9 + 7, mx = 1e6;
+ll dp[mx + 1];
+int coins[mx+1];
+int n, x;
 
 int main(){
+    cin >> n >> x;
+    for(int i = 1; i <= n;i++){
+        cin >> coins[i - 1];
+    }
+    dp[0] = 1;
+    for(int j = 0 ; j < n;j++){
+        for(int i = 1; i <= x;i++){
+            if(coins[j] <= i){
+                dp[i] = (dp[i] + dp[i - coins[j]]) % mod;
+            }
+        }
+    }
+    cout << dp[x];
     return 0;
 }
